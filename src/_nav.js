47866abfd6591b0558871038e41a20e698279186
Adestro <adestro.afk@gmail.com>
2025-01-28 09:27:46 -0500

    Títulos sidebar

--- src/_nav.js
+++ src/_nav.js
@@ -32,21 +32,30 @@ const _nav = [
     component: CNavTitle,
     name: 'Inventario',
   },
-
   {
     component: CNavItem,
     name: 'Productos',
     to: '/producto',
     icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
   },
-
   {
     component: CNavItem,
-    name: 'Categoría',
-    to: '/cotizacion',
-    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
+    name: 'Categorías',
+    to: '/categorias',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
   },
-
+  {
+    component: CNavItem,
+    name: 'Proveedores',
+    to: '/proveedores',
+    icon: <CIcon icon={cilExternalLink} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Movimientos',
+    to: '/movimientos',
+    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
+  },
 
   {
     component: CNavTitle,
@@ -56,53 +65,117 @@ const _nav = [
     component: CNavItem,
     name: 'Ventas',
     to: '/ventas',
-    icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
+    icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
-    name: 'Cotización',
-    to: '/cotización',
+    name: 'Cotizaciones',
+    to: '/cotizaciones',
     icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
   },
   {
     component: CNavItem,
     name: 'Compras',
     to: '/compras',
-    icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
+    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
   },
+
   {
     component: CNavTitle,
     name: 'Clientes',
   },
-  
-  
-  
+  {
+    component: CNavItem,
+    name: 'Lista de Clientes',
+    to: '/clientes',
+    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Créditos',
+    to: '/creditos',
+    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
+  },
+
+  {
+    component: CNavTitle,
+    name: 'Usuarios y Permisos',
+  },
+  {
+    component: CNavItem,
+    name: 'Usuarios',
+    to: '/usuarios',
+    icon: <CIcon icon={cilCursor} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Roles',
+    to: '/roles',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+  },
+
+  {
+    component: CNavTitle,
+    name: 'Reportes',
+  },
+  {
+    component: CNavItem,
+    name: 'Reportes de Ventas',
+    to: '/reportes-ventas',
+    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Reportes de Inventario',
+    to: '/reportes-inventario',
+    icon: <CIcon icon={cilChartPie} customClassName="nav-icon" />,
+  },
+
+  {
+    component: CNavTitle,
+    name: 'Configuración',
+  },
+  {
+    component: CNavItem,
+    name: 'Empresa',
+    to: '/empresa',
+    icon: <CIcon icon={cilExternalLink} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Impuestos',
+    to: '/impuestos',
+    icon: <CIcon icon={cilCalculator} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Backup',
+    to: '/backup',
+    icon: <CIcon icon={cilDescription} customClassName="nav-icon" />,
+  },
+
   {
     component: CNavTitle,
     name: 'Extras',
   },
   {
-    component: CNavGroup,
-    name: 'Pages',
-    icon: <CIcon icon={cilStar} customClassName="nav-icon" />,
-    items: [
-      {
-        component: CNavItem,
-        name: 'Register',
-        to: '/register',
-      },
-      {
-        component: CNavItem,
-        name: 'Error 404',
-        to: '/404',
-      },
-      {
-        component: CNavItem,
-        name: 'Error 500',
-        to: '/500',
-      },
-    ],
+    component: CNavItem,
+    name: 'Notificaciones',
+    to: '/notificaciones',
+    icon: <CIcon icon={cilBell} customClassName="nav-icon" />,
   },
-  ]
+  {
+    component: CNavItem,
+    name: 'Calendario',
+    to: '/calendario',
+    icon: <CIcon icon={cilNotes} customClassName="nav-icon" />,
+  },
+  {
+    component: CNavItem,
+    name: 'Soporte',
+    to: '/soporte',
+    icon: <CIcon icon={cilExternalLink} customClassName="nav-icon" />,
+  },
+];
 
-export default _nav
+export default _nav;
